3ec9b14eea392db2a09f9d64f00351c9d7aed566
Nacho4321 <ignacioarisnabarreta@gmail.com>
2025-10-27 18:18:59 -0300

    Creacion del test del protocolo

---
+++ tests/test_protocol.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+#include "../common/protocol.h"
+#include "../common/socket.h"
+#include "../common/constants.h"
+
+// Helper para iniciar servidor y cliente en puertos distintos
+static const char* TEST_PORT = "50000";
+
+// =====================================================================
+// TESTS
+// =====================================================================
+
+TEST(ProtocolLocalhostTest, SendAndReceiveUpPressed) {
+    std::thread server_thread([]() {
+        Socket listener(TEST_PORT);              // servidor escucha
+        Socket peer = listener.accept();         // bloqueante
+        Protocol proto_server(std::move(peer));  // protocolo del servidor
+        DecodedMessage msg = proto_server.receiveMessage(); // bloqueante
+        EXPECT_EQ(msg.cmd, MOVE_UP_PRESSED_STR);
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // da tiempo al server
+
+    Socket client("localhost", TEST_PORT);       // cliente conecta
+    Protocol proto_client(std::move(client));    // protocolo del cliente
+    proto_client.sendMessage(MOVE_UP_PRESSED_STR);
+
+    server_thread.join();
+}
+
+TEST(ProtocolLocalhostTest, SendAndReceiveRightReleased) {
+    std::thread server_thread([]() {
+        Socket listener(TEST_PORT);
+        Socket peer = listener.accept();
+        Protocol proto_server(std::move(peer));
+        DecodedMessage msg = proto_server.receiveMessage();
+        EXPECT_EQ(msg.cmd, MOVE_RIGHT_RELEASED_STR);
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    Socket client("localhost", TEST_PORT);
+    Protocol proto_client(std::move(client));
+    proto_client.sendMessage(MOVE_RIGHT_RELEASED_STR);
+
+    server_thread.join();
+}
+
+TEST(ProtocolLocalhostTest, MultipleSequentialMessages) {
+    std::thread server_thread([]() {
+        Socket listener(TEST_PORT);
+        Socket peer = listener.accept();
+        Protocol proto_server(std::move(peer));
+
+        auto m1 = proto_server.receiveMessage();
+        EXPECT_EQ(m1.cmd, MOVE_DOWN_PRESSED_STR);
+
+        auto m2 = proto_server.receiveMessage();
+        EXPECT_EQ(m2.cmd, MOVE_LEFT_RELEASED_STR);
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    Socket client("localhost", TEST_PORT);
+    Protocol proto_client(std::move(client));
+
+    proto_client.sendMessage(MOVE_DOWN_PRESSED_STR);
+    proto_client.sendMessage(MOVE_LEFT_RELEASED_STR);
+
+    server_thread.join();
+}
