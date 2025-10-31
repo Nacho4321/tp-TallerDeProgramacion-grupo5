c02264118724baee21c40f8c3fc1cfaf51e71191
Nacho4321 <ignacioarisnabarreta@gmail.com>
2025-10-31 13:23:57 -0300

    Creacion de archivos server_handler para abstraer la utilizacion del acceptor y queues

---
+++ server/server_handler.cpp
@@ -0,0 +1,32 @@
+#include "server_handler.h"
+
+ServerHandler::ServerHandler(const char* port)
+    : inbox(), players(), outboxes(),
+      acceptor(port, inbox, players, outboxes) {}
+
+ServerHandler::~ServerHandler() {
+    stop();
+}
+
+void ServerHandler::start() {
+    if (!started) {
+        acceptor.start();
+        started = true;
+    }
+}
+
+void ServerHandler::stop() {
+    if (started) {
+        acceptor.stop();
+        acceptor.join();
+        started = false;
+    }
+}
+
+bool ServerHandler::try_receive(ClientHandlerMessage& out) {
+    return inbox.try_pop(out);
+}
+
+void ServerHandler::broadcast(const ServerMessage& msg) {
+    acceptor.broadcast(msg);
+}
