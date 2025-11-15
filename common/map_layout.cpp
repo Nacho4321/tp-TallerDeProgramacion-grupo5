660a81266c871f6544fa5519e49f1426b101fca6
Agustín Dubovitsky <71897319+agusdubo@users.noreply.github.com>
2025-11-15 18:00:53 -0300

    fix: map_size
    Tamaño ajustado a los píxeles exactos con escala exacta

--- common/map_layout.cpp
+++ common/map_layout.cpp
@@ -1,9 +1,8 @@
 #include "map_layout.h"
 
-#define MAP_WIDTH (640.0f / 4.5f)
-#define MAP_HEIGHT (480.0f / 3.35f)
-#define WALL_THICKNESS 0.25f // grosor de la pared en metros
-
+#define MAP_WIDTH (4640.0f / 32.0f)
+#define MAP_HEIGHT (4672.0f / 32.0f)
+#define WALL_THICKNESS 0.1f
 void MapLayout::create_walls()
 {
     b2BodyDef bd;
@@ -12,25 +11,25 @@ void MapLayout::create_walls()
 
     b2PolygonShape shape_upper;
     shape_upper.SetAsBox(MAP_WIDTH / 2.0f, WALL_THICKNESS,
-                         b2Vec2(MAP_WIDTH / 2.0f, -WALL_THICKNESS),
+                         b2Vec2(MAP_WIDTH / 2.0f, WALL_THICKNESS),
                          0.0f);
     worldBounds->CreateFixture(&shape_upper, 0.0f);
 
     b2PolygonShape shape_lower;
     shape_lower.SetAsBox(MAP_WIDTH / 2.0f, WALL_THICKNESS,
-                         b2Vec2(MAP_WIDTH / 2.0f, MAP_HEIGHT + WALL_THICKNESS),
+                         b2Vec2(MAP_WIDTH / 2.0f, MAP_HEIGHT - WALL_THICKNESS * 0.5f),
                          0.0f);
     worldBounds->CreateFixture(&shape_lower, 0.0f);
 
     b2PolygonShape shape_left;
     shape_left.SetAsBox(WALL_THICKNESS, MAP_HEIGHT / 2.0f,
-                        b2Vec2(-WALL_THICKNESS, MAP_HEIGHT / 2.0f),
+                        b2Vec2(WALL_THICKNESS, MAP_HEIGHT / 2.0f),
                         0.0f);
     worldBounds->CreateFixture(&shape_left, 0.0f);
 
     b2PolygonShape shape_right;
     shape_right.SetAsBox(WALL_THICKNESS, MAP_HEIGHT / 2.0f,
-                         b2Vec2(MAP_WIDTH + WALL_THICKNESS, MAP_HEIGHT / 2.0f),
+                         b2Vec2(MAP_WIDTH - WALL_THICKNESS, MAP_HEIGHT / 2.0f),
                          0.0f);
     worldBounds->CreateFixture(&shape_right, 0.0f);
 }
